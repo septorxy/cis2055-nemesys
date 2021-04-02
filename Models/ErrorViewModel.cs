312e06164aa5346043d141e2ffeed5b55270d76e
Liam Gatt <liamgatt@gmail.com>
2021-04-02 14:07:16 +0200

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cis2055_nemesys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
