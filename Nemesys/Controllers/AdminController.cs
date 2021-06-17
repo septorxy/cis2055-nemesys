3399e4e5992c1cfd0a86daea156d416bf1e906cc
Liam Gatt <liamgatt@gmail.com>
2021-06-17 22:16:16 +0200

    Logging added

--- Nemesys/Controllers/AdminController.cs
+++ Nemesys/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Nemesys.Models;
 using Nemesys.Models.Interfaces;
 using System;
@@ -17,13 +18,15 @@ namespace Nemesys.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly INemesysRepository _nemesysRepository;
+        private readonly ILogger<AdminController> _logger;
 
-        public AdminController(INemesysRepository nemesysRepository, RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public AdminController(INemesysRepository nemesysRepository, RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ILogger<AdminController> logger)
         {
-            this._roleManager = roleManager;
-            this._userManager = userManager;
-            this._signInManager = signInManager;
-            this._nemesysRepository = nemesysRepository;
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _nemesysRepository = nemesysRepository;
+            _logger = logger;
         }
 
         public IActionResult Index()
@@ -40,12 +43,20 @@ namespace Nemesys.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateRole(ProjectRole role)
         {
-            var roleExist = await _roleManager.RoleExistsAsync(role.RoleName);
-            if (!roleExist)
+            try
             {
-                var result = await _roleManager.CreateAsync(new IdentityRole(role.RoleName));
+                var roleExist = await _roleManager.RoleExistsAsync(role.RoleName);
+                if (!roleExist)
+                {
+                    var result = await _roleManager.CreateAsync(new IdentityRole(role.RoleName));
+                }
+                return View();
             }
-            return View();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, ex.Data);
+                return View("Error");
+            }
         }
 
         [HttpGet]
@@ -57,19 +68,28 @@ namespace Nemesys.Controllers
         [HttpPost]
         public async Task<IActionResult> Assign(string role, string username)
         {
-            AppUser User = _nemesysRepository.GetUserByUsername(username);
-            if(User != null) {
-                var currRole = await _userManager.GetRolesAsync(User);
-                await _userManager.RemoveFromRoleAsync(User, currRole[0]);
-                await _userManager.AddToRoleAsync(User, role);
-            }
-            else
+            try
             {
-                ModelState.AddModelError(string.Empty, "User Not Found");
-                return View();
-            }
+                AppUser User = _nemesysRepository.GetUserByUsername(username);
+                if (User != null)
+                {
+                    var currRole = await _userManager.GetRolesAsync(User);
+                    await _userManager.RemoveFromRoleAsync(User, currRole[0]);
+                    await _userManager.AddToRoleAsync(User, role);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User Not Found");
+                    return View();
+                }
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, ex.Data);
+                return View("Error");
+            }
         }
 
         [HttpGet]
@@ -82,29 +102,37 @@ namespace Nemesys.Controllers
         [HttpPost]
         public IActionResult CreateStatus(string name)
         {
-            Boolean exists = false;
-            IEnumerable<Status> statuses = _nemesysRepository.GetAllStatuses();
-            foreach (Status status in statuses)
+            try
             {
-                if (status.Name.Equals(name))
+                Boolean exists = false;
+                IEnumerable<Status> statuses = _nemesysRepository.GetAllStatuses();
+                foreach (Status status in statuses)
                 {
-                    exists = true;
-                    break;
+                    if (status.Name.Equals(name))
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists)
+                {
+                    var newStatus = new Status()
+                    {
+                        Name = name
+                    };
+                    _nemesysRepository.CreateStatus(newStatus);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Status Already Exists");
+                    return View();
                 }
             }
-            if (!exists)
+            catch (Exception ex)
             {
-                var newStatus = new Status()
-                {
-                    Name = name
-                };
-                _nemesysRepository.CreateStatus(newStatus);
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "Status Already Exists");
-                return View();
+                _logger.LogError(ex, ex.Message, ex.Data);
+                return View("Error");
             }
         }
 
@@ -118,29 +146,37 @@ namespace Nemesys.Controllers
         [HttpPost]
         public IActionResult CreateType(string name)
         {
-            Boolean exists = false;
-            IEnumerable<Models.Type> types = _nemesysRepository.GetAllTypes();
-            foreach (Models.Type type in types)
+            try
             {
-                if (type.Name.Equals(name))
+                Boolean exists = false;
+                IEnumerable<Models.Type> types = _nemesysRepository.GetAllTypes();
+                foreach (Models.Type type in types)
                 {
-                    exists = true;
-                    break;
+                    if (type.Name.Equals(name))
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists)
+                {
+                    var newType = new Models.Type()
+                    {
+                        Name = name
+                    };
+                    _nemesysRepository.CreateType(newType);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Type Already Exists");
+                    return View();
                 }
             }
-            if (!exists)
+            catch (Exception ex)
             {
-                var newType = new Models.Type()
-                {
-                    Name = name
-                };
-                _nemesysRepository.CreateType(newType);
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "Type Already Exists");
-                return View();
+                _logger.LogError(ex, ex.Message, ex.Data);
+                return View("Error");
             }
         }
 
--- Nemesys/Controllers/ReportsController.cs
+++ Nemesys/Controllers/ReportsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Nemesys.Models;
 using Nemesys.Models.Interfaces;
 using Nemesys.ViewModels;
@@ -14,25 +15,36 @@ using System.Threading.Tasks;
 namespace Nemesys.Controllers
 {
     public class ReportsController : Controller
-    {   
+    {
         private readonly INemesysRepository _nemesysRepository;
         private readonly UserManager<AppUser> _userManager;
         private readonly IEmailSender _emailSender;
+        private readonly ILogger<ReportsController> _logger;
 
-        public ReportsController(INemesysRepository nemesysRepository, UserManager<AppUser> userManager, IEmailSender emailSender)
+        public ReportsController(INemesysRepository nemesysRepository, UserManager<AppUser> userManager, IEmailSender emailSender, ILogger<ReportsController> logger)
         {
             _nemesysRepository = nemesysRepository;
             _userManager = userManager;
             _emailSender = emailSender;
+            _logger = logger;
         }
 
         public IActionResult Index()
         {
-            var model = new ReportListViewModel() {
-                TotalEntries = _nemesysRepository.GetAllReports().Count(),
-                Reports = _nemesysRepository.GetAllReports().OrderByDescending(b => b.ReportDate)
-            };
-            return View(model);
+            try
+            {
+                var model = new ReportListViewModel()
+                {
+                    TotalEntries = _nemesysRepository.GetAllReports().Count(),
+                    Reports = _nemesysRepository.GetAllReports().OrderByDescending(b => b.ReportDate)
+                };
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, ex.Data);
+                return View("Error");
+            }
         }
 
         public IActionResult Details(int id)
@@ -45,7 +57,7 @@ namespace Nemesys.Controllers
                 Status currStatus = _nemesysRepository.GetStatusById(report.StatusId);
                 Models.Type currType = _nemesysRepository.GetTypeById(report.TypeId);
                 var getVote = _nemesysRepository.getVoted(report.Id, _userManager.GetUserId(User));
-                if(getVote != null)
+                if (getVote != null)
                 {
                     currVote = getVote.vote;
                 }
@@ -60,12 +72,14 @@ namespace Nemesys.Controllers
                         ReportDate = report.ReportDate,
                         HazardDate = report.HazardDate,
                         Location = report.Location,
-                        Type = new ListViewModel() {
+                        Type = new ListViewModel()
+                        {
                             Id = currType.Id,
                             Name = currType.Name
                         },
                         Description = report.Description,
-                        Status = new ListViewModel() {
+                        Status = new ListViewModel()
+                        {
                             Id = currStatus.Id,
                             Name = currStatus.Name
                         },
@@ -131,7 +145,8 @@ namespace Nemesys.Controllers
                 }
             }
             catch (Exception ex)
-            { 
+            {
+                _logger.LogError(ex, ex.Message, ex.Data);
                 return View("Error");
             }
         }
@@ -140,77 +155,93 @@ namespace Nemesys.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            var typeList = _nemesysRepository.GetAllTypes().Select(t => new ListViewModel()
-            {
-                Id = t.Id,
-                Name = t.Name
-            }).ToList();
-            var model = new EditReportViewModel()
-            {
-                TypeList = typeList
-            };
-            return View(model);
-        }
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult Create([Bind("Description, TypeId, ImageToUpload, Location, HazardDate, Longitude, Latitude")] EditReportViewModel newReport)
-        {
-            if (ModelState.IsValid)
-            {
-                string fileName = "";
-                if (newReport.ImageToUpload != null)
-                {
-                    var extension = "." + newReport.ImageToUpload.FileName.Split('.')[newReport.ImageToUpload.FileName.Split('.').Length - 1];
-                    fileName = Guid.NewGuid().ToString() + extension;
-                    var path = Directory.GetCurrentDirectory() + "\\wwwroot\\images\\reports\\" + fileName;
-                    using (var bits = new FileStream(path, FileMode.Create))
-                    {
-                        newReport.ImageToUpload.CopyTo(bits);
-                    }
-                }
-
-                Report report = new Report()
-                {
-                    TypeId = newReport.TypeId,
-                    Description = newReport.Description,
-                    Location = newReport.Location,
-                    HazardDate = newReport.HazardDate,
-                    ReportDate = DateTime.UtcNow,
-                    StatusId = 1,
-                    PhotoUrl = " /images/reports/" + fileName,
-                    Upvotes = 0,
-                    UserId = _userManager.GetUserId(User),
-                    Longitude = newReport.Longitude,
-                    Latitude = newReport.Latitude
-                };
-
-                _nemesysRepository.CreateReport(report);
-                var user = _nemesysRepository.GetUserByUsername(User.Identity.Name);
-                _nemesysRepository.UpdateTotalReports(user, 1);
-                //var AllUsers = _nemesysRepository.GetAllUsers();
-                //foreach(var aUser in AllUsers)
-                //{
-                //    var role = await _userManager.GetRolesAsync(aUser);
-                //    if (role.Equals("Investigator") || role.Equals("Admin"))
-                //    {
-                //        if(user != aUser)
-                //            await _emailSender.SendEmailAsync(aUser.Email, "Attention! A Report has been created", $"Dear {report.User.UserName},<br>Please refer to <a href='https://universitynemesys.azurewebsites.net/Reports/Details/" + report.Id + "'>this link</a> to view the new report.<br>Sincerely,<br>The Admin Team");
-                //    }
-                //}
-                return RedirectToAction("Index");
-            }
-            else
+            try
             {
                 var typeList = _nemesysRepository.GetAllTypes().Select(t => new ListViewModel()
                 {
                     Id = t.Id,
                     Name = t.Name
                 }).ToList();
-                newReport.TypeList = typeList;
-                newReport.Longitude = 0;
-                newReport.Latitude = 0;
-                return View(newReport);
+                var model = new EditReportViewModel()
+                {
+                    TypeList = typeList
+                };
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, ex.Data);
+                return View("Error");
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Create([Bind("Description, TypeId, ImageToUpload, Location, HazardDate, Longitude, Latitude")] EditReportViewModel newReport)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string fileName = "";
+                    if (newReport.ImageToUpload != null)
+                    {
+                        var extension = "." + newReport.ImageToUpload.FileName.Split('.')[newReport.ImageToUpload.FileName.Split('.').Length - 1];
+                        fileName = Guid.NewGuid().ToString() + extension;
+                        var path = Directory.GetCurrentDirectory() + "\\wwwroot\\images\\reports\\" + fileName;
+                        using (var bits = new FileStream(path, FileMode.Create))
+                        {
+                            newReport.ImageToUpload.CopyTo(bits);
+                        }
+                    }
+
+                    Report report = new Report()
+                    {
+                        TypeId = newReport.TypeId,
+                        Description = newReport.Description,
+                        Location = newReport.Location,
+                        HazardDate = newReport.HazardDate,
+                        ReportDate = DateTime.UtcNow,
+                        StatusId = 1,
+                        PhotoUrl = " /images/reports/" + fileName,
+                        Upvotes = 0,
+                        UserId = _userManager.GetUserId(User),
+                        Longitude = newReport.Longitude,
+                        Latitude = newReport.Latitude
+                    };
+
+                    _nemesysRepository.CreateReport(report);
+                    var user = _nemesysRepository.GetUserByUsername(User.Identity.Name);
+                    _nemesysRepository.UpdateTotalReports(user, 1);
+                    //var AllUsers = _nemesysRepository.GetAllUsers();
+                    //foreach(var aUser in AllUsers)
+                    //{
+                    //    var role = await _userManager.GetRolesAsync(aUser);
+                    //    if (role.Equals("Investigator") || role.Equals("Admin"))
+                    //    {
+                    //        if(user != aUser)
+                    //            await _emailSender.SendEmailAsync(aUser.Email, "Attention! A Report has been created", $"Dear {report.User.UserName},<br>Please refer to <a href='https://universitynemesys.azurewebsites.net/Reports/Details/" + report.Id + "'>this link</a> to view the new report.<br>Sincerely,<br>The Admin Team");
+                    //    }
+                    //}
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var typeList = _nemesysRepository.GetAllTypes().Select(t => new ListViewModel()
+                    {
+                        Id = t.Id,
+                        Name = t.Name
+                    }).ToList();
+                    newReport.TypeList = typeList;
+                    newReport.Longitude = 0;
+                    newReport.Latitude = 0;
+                    return View(newReport);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, ex.Data);
+                return View("Error");
             }
         }
 
@@ -255,7 +286,7 @@ namespace Nemesys.Controllers
             }
             catch (Exception ex)
             {
-                
+                _logger.LogError(ex, ex.Message, ex.Data);
                 return View("Error");
             }
         }
@@ -295,7 +326,7 @@ namespace Nemesys.Controllers
                             photoUrl = "/images/reports/" + fileName;
                         }
                         else
-                           photoUrl = modelToUpdate.PhotoUrl;
+                            photoUrl = modelToUpdate.PhotoUrl;
 
                         modelToUpdate.TypeId = editedReport.TypeId;
                         modelToUpdate.Description = editedReport.Description;
@@ -323,7 +354,7 @@ namespace Nemesys.Controllers
 
                         return View(editedReport);
                     }
-                        
+
                 }
                 else
                 {
@@ -332,7 +363,7 @@ namespace Nemesys.Controllers
             }
             catch (Exception ex)
             {
-
+                _logger.LogError(ex, ex.Message, ex.Data);
                 return View("Error");
             }
         }
@@ -365,7 +396,7 @@ namespace Nemesys.Controllers
 
                     return RedirectToAction("Index");
 
-                   
+
                 }
                 else
                 {
@@ -375,7 +406,7 @@ namespace Nemesys.Controllers
             }
             catch (Exception ex)
             {
-
+                _logger.LogError(ex, ex.Message, ex.Data);
                 return View("Error");
             }
         }
@@ -394,7 +425,7 @@ namespace Nemesys.Controllers
 
                 //Check if the current user has access to this resource
                 var currentUser = await _userManager.GetUserAsync(User);
-                if(_nemesysRepository.getVoted(id, currentUser.Id) == null) 
+                if (_nemesysRepository.getVoted(id, currentUser.Id) == null)
                 {
                     modelToUpvote.Upvotes++;
                 }
@@ -405,19 +436,19 @@ namespace Nemesys.Controllers
 
                 Vote vote = new Vote()
                 {
-                   ReportId = id,
-                   UserId = currentUser.Id,
-                   vote = 1
+                    ReportId = id,
+                    UserId = currentUser.Id,
+                    vote = 1
                 };
                 _nemesysRepository.setVoted(vote);
                 _nemesysRepository.UpdateReport(modelToUpvote);
                 return RedirectToAction("Details", new { Id = id });
 
-                
+
             }
             catch (Exception ex)
             {
-
+                _logger.LogError(ex, ex.Message, ex.Data);
                 return View("Error");
             }
         }
@@ -460,7 +491,7 @@ namespace Nemesys.Controllers
             }
             catch (Exception ex)
             {
-
+                _logger.LogError(ex, ex.Message, ex.Data);
                 return View("Error");
             }
         }
